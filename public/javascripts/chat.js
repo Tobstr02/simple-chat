0c54905e14c9296ceca8a828112cd6fdaf9e5cec
Tobias Hopp <tobi@gaminggeneration.de>
2023-04-18 13:50:45 +0200

    add chat.js

---
+++ public/javascripts/chat.js
@@ -0,0 +1,24 @@
+/**
+ * Willis Code
+ */
+
+
+
+/**
+ * Sobald ein Chat hereinkommt
+ * @param data {username: string, message: string}
+ */
+function onChat(data)
+{
+	// todo Hier divs zur chatbox hinzufügen mit "Name: Message"
+}
+
+
+/**
+ * Sobald sich ein active state eines users ändert
+ * @param data {username: string, active: boolean}
+ */
+function onState(data)
+{
+	// todo Hier divs zur chatbox vllt andere Farbe mit "Name hat verlassen" (bei active=false) bzw. "Name hat betreten" (bei active=true)
+}
